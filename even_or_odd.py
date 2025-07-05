24af47f77e5e24efb692620324f55c2c5e06511e
Heet Asodia <heet2511@gmail.com>
2025-07-05 19:50:17 +0530

    Create even_or_odd.py
    Check any number is even or odd

---
+++ even_or_odd.py
@@ -0,0 +1,10 @@
+#Check no is even or odd.
+def is_even(n): 
+   return n % 2 == 0
+
+def checkoddeven(number):
+    if number % 2 == 0:
+       return "Even"
+    else:
+        return "Odd" 
+print(checkoddeven(10))
